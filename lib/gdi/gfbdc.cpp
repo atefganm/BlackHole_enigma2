0a696c0edbe0d249feb1c3cd0f3d56e28751bfea
atefganm <atef_anim_1974@yahoo.com>
2026-09-14 22:14:04 +0300

    fixed framebuffer for current 7080, dm820, dm900, dm520 and  use ion to allocate accel memory
    Co-Authored-By: ghost <andreas.monzner@dream-property.net>

--- lib/gdi/gfbdc.cpp
+++ lib/gdi/gfbdc.cpp
@@ -34,9 +34,10 @@ extern void  dinibot_accel_notify(void);
 gFBDC::gFBDC()
 {
 	fb=new fbClass;
-
+#ifndef CONFIG_ION
 	if (!fb->Available())
 		eFatal("[gFBDC] no framebuffer available");
+#endif
 
 	int xres;
 	int yres;
@@ -258,10 +259,10 @@ void gFBDC::setResolution(int xres, int yres, int bpp)
 	#endif
 		)
 		return;
-
+#ifndef CONFIG_ION
 	if (gAccel::getInstance())
 		gAccel::getInstance()->releaseAccelMemorySpace();
-
+#endif
 	fb->SetMode(xres, yres, bpp);
 
 	surface.x = xres;
@@ -294,8 +295,12 @@ void gFBDC::setResolution(int xres, int yres, int bpp)
 	eDebug("[gFBDC] resolution: %dx%dx%d stride=%d, %dkB available for acceleration surfaces.",
 		 surface.x, surface.y, surface.bpp, fb->Stride(), (fb->Available() - fb_size)/1024);
 
+#ifndef CONFIG_ION
+	/* accel is already set in fb.cpp */
+	eDebug("%dkB available for acceleration surfaces.", (fb->Available() - fb_size)/1024);
 	if (gAccel::getInstance())
 		gAccel::getInstance()->setAccelMemorySpace(fb->lfb + fb_size, surface.data_phys + fb_size, fb->Available() - fb_size);
+#endif
 
 #ifdef HAVE_HISILICON_ACCEL
 	dinobot_accel_register(&surface,&surface_back);
